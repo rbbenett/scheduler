bce5aa0998192ff9b5294ff06a741016be306fc9
Robert <rbbenett0614@gmail.com>
2021-01-18 19:45:54 -0500

    Added DayList, DayListItem, Button and updated Application to render data

---
+++ src/components/DayListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "components/DayListItem.scss";
+
+let classNames = require('classnames');
+
+export default function DayListItem(props) { 
+  var dayListItemClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots === 0
+  });
+  
+  const formatSpots = (spots) => {
+    if (spots === 0) {
+      return 'no spots remaining'
+    } else if (spots === 1) {
+      return '1 spot remaining'
+    } else {
+      return `${spots} spots remaining`
+    }
+  }
+
+  return (
+    <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+};
+
